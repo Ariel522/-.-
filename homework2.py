0e65381afb47059ea7ef3c3ae39c8af9f7ea1bdf
Ariel522 <arik9211@mail.ru>
2024-07-06 10:55:05 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,12 @@
+# Peremennie
+a = ('Курус:')
+b = ('всего задач:')
+c = ('зтрачено часов:')
+d = ('среднее время выполнения ')
+e = ('часа')
+
+nazvanie_kursa = ('Python')
+vsego_zadash = ('12,')
+zatrash_shasov = ('1.5,')
+shrednee = float(1.5 / 12 )
+print (a, nazvanie_kursa, b, vsego_zadash, c, zatrash_shasov, d, shrednee,e)
